a412dbc5621362caba3e7e459c70a0dcbe939fb6
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-06-20 14:57:08 -0300

    POST sign-up

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from 'express'
+import cors from 'cors'
+
+const server = express()
+server.use(cors())
+
+
+let usuario ={
+    username: '',
+    avatar: ''
+}
+
+let tweets = []
+
+let tweet = {
+    username: '',
+    tweet: '',
+    avatar: ''
+}
+
+server.use(express.json());
+
+server.post("/sign-up", (req, res) => {
+    if (req.body.username && req.body.avatar) {
+        const usuarioRegistrado = usuario.find(
+            (usuario) => usuario.username === req.body.username
+        );
+        if (usuario) {
+            res.status(400).send('Usuário já existe');
+        } else {
+            usuario.push(req.body);            
+            res.status(201).send('OK');
+        }
+    } else {
+        res.status(400).send('Falta nome de usuário ou avatar');
+    }     
+});
+
+
+server.post("/tweets", (req, res) => {
+    
+    const tweetMensagem = req.body.tweet;
+    const { usuário } = req.headers;
+    if (tweetMensagem && usuário) {
+        tweet.push(req.body)
+        const userTweet = users.find(
+            (usuario) => usuario.username === tweet.username
+        );
+        tweet = { ...tweet, avatar: userTweet.avatar };
+        tweets.push(tweet);
+        res.status(201).send('OK');
+    } else {
+        res.status(400).send('Falta nome de usuário ou tweet');
+    }
+});
+
+server.get("/", (req, res) => {        
+    res.send("ola")
+});
+
+
+server.get("/tweets", (req, res) => {        
+    res.send(tweets)
+});
+
+server.listen(5000)
+
